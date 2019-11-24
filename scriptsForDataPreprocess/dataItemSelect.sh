fab43e3dbd9becffe3d0e91d8ecf583ac36f9035
zhanghuoding <wangbobochn@gmail.com>
2019-11-24 17:10:24 +0800

    Ckecking data files.

--- scriptsForDataPreprocess/dataItemSelect.sh
+++ scriptsForDataPreprocess/dataItemSelect.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 #This shell file will help me to select the item I need in file bach_instance.csv.
+
+dataPath="/mnt/shared/Datasets/bobowang/For_this_Experiment"
+dataPath_2017=$dataPath+"/trace_201708"
+dataPath_2018=$dataPath+"/alibaba_clusterdata_v2018"
+
+ls -alh $dataPath
+
+ls -alh $dataPath2017
+ls -alh $dataPath2018

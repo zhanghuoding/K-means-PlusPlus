768ba7de9e98a182da5922fdb604600209c9efbf
zhanghuoding <wangbobochn@gmail.com>
2019-11-24 17:26:57 +0800

    Test`.

--- scriptsForDataPreprocess/dataItemSelect.sh
+++ scriptsForDataPreprocess/dataItemSelect.sh
@@ -3,10 +3,10 @@
 #This shell file will help me to select the item I need in file bach_instance.csv.
 
 dataPath="/mnt/shared/Datasets/bobowang/For_this_Experiment"
-dataPath_2017=$dataPath+"/trace_201708"
-dataPath_2018=$dataPath+"/alibaba_clusterdata_v2018"
+dataPath_2017=$dataPath"/trace_201708"
+dataPath_2018=$dataPath"/alibaba_clusterdata_v2018"
 
-ls -alh $dataPath
-
-ls -alh $dataPath2017
-ls -alh $dataPath2018
+#First deal with dataset "batch_instance.csv" at 2017.
+echo $dataPath_2017
+ls -alh $dataPath_2017
+cp $dataPath_2017/batch_instance.csv $dataPath_2017/batch_instance-with_heads.csv

225c5f51b573f2fd1fbdcf1a0dbc37df299ea2a0
zhanghuoding <wangbobochn@gmail.com>
2019-11-24 19:09:46 +0800

    a

--- scriptsForDataPreprocess/dataItemSelect.sh
+++ scriptsForDataPreprocess/dataItemSelect.sh
@@ -10,6 +10,9 @@ dataPath_2018=$dataPath"/alibaba_clusterdata_v2018"
 
 ls -alh $dataPath_2017
 #cp $dataPath_2017/batch_instance.csv $dataPath_2017/batch_instance-with_heads.csv
-head $dataPath_2017/batch_instance-with_heads.csv
-sed -i '1 istart_timestamp,end_timestamp,job_id,task_id,machineID,status,seq_no,total_seq_no,real_cpu_max,real_cpu_avg,real_mem_max,real_mem_avg' $dataPath_2017/batch_instance-with_heads.csv
-head $dataPath_2017/batch_instance-with_heads.csv
+#head $dataPath_2017/batch_instance-with_heads.csv
+#sed -i '1 istart_timestamp,end_timestamp,job_id,task_id,machineID,status,seq_no,total_seq_no,real_cpu_max,real_cpu_avg,real_mem_max,real_mem_avg' $dataPath_2017/batch_instance-with_heads.csv
+#head $dataPath_2017/batch_instance-with_heads.csv
+awk -F , 'END{print "lines="NR;}'  $dataPath_2017/batch_instance.csv $dataPath_2017/batch_instance.csv
+awk -F , 'END{print "lines="NR;}'  $dataPath_2017/batch_instance.csv $dataPath_2017/batch_instance-with_heads.csv
+csvkit

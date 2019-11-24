8c0827e33e2f9c2ae1a7f0bb137ba684589c6690
zhanghuoding <wangbobochn@gmail.com>
2019-11-24 21:34:49 +0800

    Testting python modules in this docker image.

--- scriptsForDataPreprocess/dataItemSelect.sh
+++ scriptsForDataPreprocess/dataItemSelect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#This shell file will help me to select the item I need in file bach_instance.csv.
+#This shell file will help me to select the item I need in file batch_instance.csv.
 
 dataPath="/mnt/shared/Datasets/bobowang/For_this_Experiment"
 dataPath_2017=$dataPath"/trace_201708"
@@ -8,7 +8,7 @@ dataPath_2018=$dataPath"/alibaba_clusterdata_v2018"
 
 #First deal with dataset "batch_instance.csv" at 2017.
 
-ls -alh $dataPath_2017
+#ls -alh $dataPath_2017
 #cp $dataPath_2017/batch_instance.csv $dataPath_2017/batch_instance-with_heads.csv
 #head $dataPath_2017/batch_instance-with_heads.csv
 #sed -i '1 istart_timestamp,end_timestamp,job_id,task_id,machineID,status,seq_no,total_seq_no,real_cpu_max,real_cpu_avg,real_mem_max,real_mem_avg' $dataPath_2017/batch_instance-with_heads.csv
@@ -17,4 +17,46 @@ ls -alh $dataPath_2017
 #awk -F , 'END{print "lines="NR;}'  $dataPath_2017/batch_instance-with_heads.csv
 #pip --version
 #pip install csvkit
-head $dataPath_2017/batch_instance-with_heads.csv | csvlook
+#head $dataPath_2017/batch_instance-with_heads.csv | csvlook
+
+
+#We will select the item from file "batch_instance.csv".
+tee -i $dataPath/test.py <<-"EOF"
+import os
+import time
+import sys
+import StringIO
+from datetime import *
+from datetime import datetime, tzinfo,timedelta
+import hashlib
+import hmac
+import hashlib
+import base64
+import logging
+from logging.handlers import RotatingFileHandler
+import subModule
+import json
+import logging.config
+import subprocess
+import xml.etree.ElementTree
+import configparser
+import random
+import string
+import shutil
+import paramiko
+import urllib.request
+import requests
+import tkinter
+import xlrd
+import math
+import networkx
+import matplotlib.pyplot
+
+exit()
+EOF
+
+cat $dataPath/test.py
+
+python $dataPath/test.py
+
+

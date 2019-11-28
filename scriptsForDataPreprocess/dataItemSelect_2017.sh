b75d59ea0c52df3af9745bf7e574783a24e3efec
zhanghuoding <wangbobochn@gmail.com>
2019-11-28 11:40:37 +0800

    i

--- scriptsForDataPreprocess/dataItemSelect_2017.sh
+++ scriptsForDataPreprocess/dataItemSelect_2017.sh
@@ -14,10 +14,10 @@ dataPath_2018=$dataPath"/alibaba_clusterdata_v2018"
 #sed -i '1 istart_timestamp,end_timestamp,job_id,task_id,machineID,status,seq_no,total_seq_no,real_cpu_max,real_cpu_avg,real_mem_max,real_mem_avg' $dataPath_2017/batch_instance-with_heads.csv
 #echo "============================================="
 #head $dataPath_2017/batch_instance-with_heads.csv
-#awk -F , 'END{print "lines="NR;}'  $dataPath_2017/batch_instance.csv
+awk -F , 'END{print "batch_instance.csv-lines="NR;}'  $dataPath_2017/batch_instance.csv
 #awk -F , 'END{print "lines="NR;}'  $dataPath_2017/batch_instance-with_heads.csv
 
-#We will select the item from file "batch_instance.csv".
-#echo "" > $dataPath_2017/batch_instance_just_selected.csv
-#awk -F , 'BEGIN{ OFS=","} { if( $1 > 0 && $2 > $1 && $6 == "Terminated" && $9 > 0 && $10 > 0 && $11 > 0 && $12 > 0 ) { print $1,$2,$9,$10,$11,$12  >> "'${dataPath_2017}'/batch_instance_just_selected.csv"; } }' ${dataPath_2017}/batch_instance.csv
-
+#We will select the item from file "batch_instance.csv" and then calculate duration for each instance.
+echo "" > $dataPath_2017/batch_instance-effective.csv
+awk -F , 'BEGIN{ OFS=","} { if( $1 > 0 && $2 > $1 && $6 == "Terminated" && $9 > 0 && $10 > 0 && $11 > 0 && $12 > 0 ) { print $2-$1,$9,$10,$11,$12  >> "'${dataPath_2017}'/batch_instance-effective.csv"; } }' ${dataPath_2017}/batch_instance.csv
+awk -F , 'END{print "batch_instance-effective.csv-lines="NR;}'  $dataPath_2017/batch_instance-effective.csv
--- scriptsForDataPreprocess/dataItemSelect_2018.sh
+++ scriptsForDataPreprocess/dataItemSelect_2018.sh
@@ -6,18 +6,19 @@ dataPath="/mnt/shared/Datasets/bobowang/For_this_Experiment"
 dataPath_2017=$dataPath"/trace_201708"
 dataPath_2018=$dataPath"/alibaba_clusterdata_v2018"
 
-#First deal with dataset "batch_instance.csv" at 2017.
+#First deal with dataset "batch_instance.csv" at 2018.
 
-#ls -alh $dataPath_2017
-#cp $dataPath_2017/batch_instance.csv $dataPath_2017/batch_instance-with_heads.csv
-#head $dataPath_2017/batch_instance-with_heads.csv
-#sed -i '1 istart_timestamp,end_timestamp,job_id,task_id,machineID,status,seq_no,total_seq_no,real_cpu_max,real_cpu_avg,real_mem_max,real_mem_avg' $dataPath_2017/batch_instance-with_heads.csv
+#ls -alh $dataPath_2018
+#cp $dataPath_2018/batch_instance.csv $dataPath_2018/batch_instance-with_heads.csv
+#head $dataPath_2018/batch_instance-with_heads.csv
+#sed -i '1 istart_timestamp,end_timestamp,job_id,task_id,machineID,status,seq_no,total_seq_no,real_cpu_max,real_cpu_avg,real_mem_max,real_mem_avg' $dataPath_2018/batch_instance-with_heads.csv
 #echo "============================================="
-#head $dataPath_2017/batch_instance-with_heads.csv
-#awk -F , 'END{print "lines="NR;}'  $dataPath_2017/batch_instance.csv
-#awk -F , 'END{print "lines="NR;}'  $dataPath_2017/batch_instance-with_heads.csv
+#head $dataPath_2018/batch_instance-with_heads.csv
+awk -F , 'END{print "batch_instance.csv_2018-lines="NR;}'  $dataPath_2018/batch_instance.csv
+#awk -F , 'END{print "lines="NR;}'  $dataPath_2018/batch_instance-with_heads.csv
 
-#We will select the item from file "batch_instance.csv".
-#echo "" > $dataPath_2017/batch_instance_just_selected.csv
-#awk -F , 'BEGIN{ OFS=","} { if( $1 > 0 && $2 > $1 && $6 == "Terminated" && $9 > 0 && $10 > 0 && $11 > 0 && $12 > 0 ) { print $1,$2,$9,$10,$11,$12  >> "'${dataPath_2017}'/batch_instance_just_selected.csv"; } }' ${dataPath_2017}/batch_instance.csv
+#We will select the item from file "batch_instance.csv", and then calculate duration for each instance.
+echo "" > $dataPath_2018/batch_instance-effective.csv
+awk -F , 'BEGIN{ OFS=","} { if( $6 > 0 && $7 > $6 && $5 == "Terminated" && $11 > 0 && $12 > 0 && $13 > 0 && $14 > 0 ) { print $7-$6,$11,$12,$13,$14  >> "'${dataPath_2018}'/batch_instance-effective.csv"; } }' ${dataPath_2018}/batch_instance.csv
+awk -F , 'END{print "batch_instance-effective.csv_2018-lines="NR;}'  $dataPath_2018/batch_instance-effective.csv
 

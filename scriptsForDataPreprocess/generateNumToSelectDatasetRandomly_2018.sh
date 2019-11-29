1d2bd9927ea221310c8a7864fe85ddca9d5c6375
zhanghuoding <wangbobochn@gmail.com>
2019-11-29 16:23:58 +0800

    add two shell scripts.

---
+++ scriptsForDataPreprocess/generateNumToSelectDatasetRandomly_2018.sh
@@ -0,0 +1,54 @@
+#/bin/bash
+
+dataPath="/mnt/shared/Datasets/bobowang/For_this_Experiment"
+dataPath_2017=$dataPath"/trace_201708"
+dataPath_2018=$dataPath"/alibaba_clusterdata_v2018"
+
+randomNumFold="$dataPath_2018/randomNumFold"
+randomNumFile="${randomNumFold}/set_"
+
+mkdir -p $randomNumFold
+
+maxNum=1907064
+
+times=10
+
+currentIndex=-1
+i=0
+while [ $i -le $times ]
+do
+	i=`expr $i + 1`
+
+	currentIndex=-1
+	ranArray[1]=-2
+	j=1
+	temp=-1
+	echo "" > $randomNumFile$i
+
+	while [ $j -le $maxNum ]
+	do
+		temp=` head -200 /dev/urandom | cksum | cut -d " " -f1 `
+		temp=`expr $temp % $maxNum`
+
+		for n in ${ranArray[*]}
+		do
+			if [ $temp == $n ]
+			then
+				temp=-1
+				break
+			fi
+		done
+
+		if [[ $temp == -1 ]]
+		then
+			continue
+		else
+			currentIndex=`expr $currentIndex + 1`
+			ranArray[$currentIndex]=$temp
+			echo $temp >> $randomNumFile$i
+			temp=-1
+			j=`expr $j + 1`
+		fi
+	done
+	unset ranArray
+done

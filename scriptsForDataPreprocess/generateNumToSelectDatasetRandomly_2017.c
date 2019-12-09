7e6c38ded4a7b79ffd82b8a9e97cd167be33b216
zhanghuoding <wangbobochn@gmail.com>
2019-12-09 21:24:56 +0800

    Multi-thread creat random numbers.

--- scriptsForDataPreprocess/generateNumToSelectDatasetRandomly_2017.c
+++ scriptsForDataPreprocess/generateNumToSelectDatasetRandomly_2017.c
@@ -1,7 +1,7 @@
 //################################################################
 //#  author   :Bobo Wang                                         #
 //#  time     :2019-11-20                                        #
-//#  modify   :2019-12-05                                        #
+//#  modify   :2019-12-09                                        #
 //#  site     :Yunnan University                                 #
 //#  e-mail   :wangbobochn@gmail.com                             #
 //################################################################
@@ -18,6 +18,8 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+#define MAX_THREAD_NUM 200
+
 #define BUFFER_SIZE 1024
 #define ARRAY_SIZE_ADDED 200
 #define ELEMENT_LENGTH 48
@@ -33,31 +35,59 @@
 char *  dataPath="/mnt/shared/Datasets/bobowang/For_this_Experiment";//path of the dataset.
 //char *  dataPath=".";//path of the dataset.
 
+int thread_count=0;//线程计数器
+pthread_t threads[MAX_THREAD_NUM];
+int threads_head_index=0;
+int threads_tail_index=0;
+
 char dataPath_2017[GENERAL_SIZE]={'\0'};
 char dataPath_2018[GENERAL_SIZE]={'\0'};
-char randomNumFold[GENERAL_SIZE] = {'\0'};
-char randomNumFile[GENERAL_SIZE] = {'\0'};
-char randomNumFileOutput[GENERAL_SIZE] = {'\0'};
-int fileNum = 0;
 
-char buffer[BUFFER_SIZE]={'\0'};
-
-unsigned long ranArray[MAXNUM + 1]={0};
+void child_thread(char*);
 
 int main( int argc, char *argv[] )
 {
+	sprintf( dataPath_2017, "%s/trace_201708", dataPath );
+	sprintf( dataPath_2018, "%s/alibaba_clusterdata_v2018", dataPath );
+
+	if( argc >= 2 )
+	{
+		int argv_i = 1;
+		while( argv_i < argc )
+		{
+			if (pthread_create(threads + (thread_count++), NULL, (void *)child_thread, (void *)argv[argv_i++] ) != 0)
+			{
+				printf("Create thread Failed!\n");
+				exit( 1 );
+			}
+			pthread_join( threads[ thread_count - 1 ], NULL );
+		}
+	}
+	else
+	{
+		printf("Please input atleast onr parameter!\n");
+		exit( 1 );
+	}
+	return 0;
+}
+
+void child_thread(char * fn)
+{
+	char randomNumFold[GENERAL_SIZE] = {'\0'};
+	char randomNumFile[GENERAL_SIZE] = {'\0'};
+	char randomNumFileOutput[GENERAL_SIZE] = {'\0'};
+	int fileNum = atoi( (char*)fn );
+
+	char buffer[BUFFER_SIZE]={'\0'};
+
+	unsigned long ranArray[MAXNUM + 1]={0};
+
 	/*
 	 *This program need one or zero parameters.
 	 */
-	if( argc == 2 )
-	{
-		fileNum = atoi( argv[1] );
-	}
 
 	srand( (unsigned)time(NULL) );
 
-	sprintf( dataPath_2017, "%s/trace_201708", dataPath );
-	sprintf( dataPath_2018, "%s/alibaba_clusterdata_v2018", dataPath );
 	sprintf( randomNumFold, "%s/randomNumFold", dataPath_2017 );
 	sprintf( randomNumFile, "%s/set_", randomNumFold );
 
@@ -78,7 +108,9 @@ int main( int argc, char *argv[] )
 
 	FILE *output = NULL;
 
-	while( i <= TIMES )
+	i = fileNum;
+	//while( i <= TIMES )
+	while( i == fileNum )
 	{
 		if( fileNum > 0 && fileNum <= TIMES )
 		{
@@ -88,6 +120,9 @@ int main( int argc, char *argv[] )
 			if( access( randomNumFileOutput, F_OK ) || access( randomNumFileOutput, R_OK ) )
 			{//if file does not exists or un-readable.
 				fileNum = -1;
+				//#############################
+				goto goon;
+				//#############################
 				continue;
 			}
 			FILE *fdata = NULL;
@@ -132,11 +167,15 @@ int main( int argc, char *argv[] )
 			else
 			{
 				currentIndex= 1;
+				//#############################
+				goto goon;
+				//#############################
 				continue;
 			}
 		}
 		else
 		{
+			goon:
 			currentIndex = 1;
 			j = 1;
 			memset( randomNumFileOutput, 0, GENERAL_SIZE);

b0d8df6aea86e4962e50c83d8393cbea73cad022
zhanghuoding <wangbobochn@gmail.com>
2020-01-13 20:17:30 +0800

    iiii

--- scriptsForDataPreprocess/dataItemSelect_2018.sh
+++ scriptsForDataPreprocess/dataItemSelect_2018.sh
@@ -27,27 +27,27 @@ ls -alh $dataPath_2018
 #keep the order the same to 201708,so "print $7-$6,$12,$11,$14,$13"
 #awk -F , 'BEGIN{ OFS=","} { if( $6 > 0 && $7 > $6 && $5 == "Terminated" && $11 > 0 && $12 > 0 && $13 > 0 && $14 > 0 ) { print $7-$6,$12,$11,$14,$13  >> "'${dataPath_2018}'/batch_instance-effective.csv"; } }' ${dataPath_2018}/batch_instance.csv
 #awk -F , 'END{print "batch_instance-effective.csv_2018-lines="NR;}'  ${dataPath_2018}/batch_instance-effective.csv
-#
-##2019-12-05-002
-#cp ${dataPath_2018}/batch_instance-effective.csv ${dataPath_2018}/batch_instance-effective.csv.backup
-#mkdir -p ${dataPath_2018}/midstFile
-#make
-#i=1
-#j=1
-#while [ $i -le $columnNum ]
-#do
-#	$currentPath/k_meanspp ${dataPath_2018}/batch_instance-effective.csv.backup ${j} $clusterNum
-#	rm -f ${dataPath_2018}/batch_instance-effective.csv.backup
-#	cp ${dataPath_2018}/clustered_column_${j}_based-batch_instance-effective.csv.backup ${dataPath_2018}/batch_instance-effective.csv.backup
-#	mv ${dataPath_2018}/clustered_column_${j}_based-batch_instance-effective.csv.backup ${dataPath_2018}/midstFile/
-#	head -300 ${dataPath_2018}/midstFile/clustered_column_${j}_based-batch_instance-effective.csv.backup >  ${dataPath_2018}/midstFile/clustered_column_${j}_based-batch_instance-effective.csv.backup-head-300.csv
-#	i=`expr $i + 1`
-#	j=`expr ${j} + 2`
-#done
-#awk -F , 'BEGIN{ OFS=","} { print $2,$4,$6,$8,$10  >> "'${dataPath_2018}'/batch_instance-usable.csv"; } END{ print "Printed total lines = "NR}' ${dataPath_2018}/batch_instance-effective.csv.backup
-#rm -f ${dataPath_2018}/batch_instance-effective.csv.backup
-#
-#
+
+#2019-12-05-002
+cp ${dataPath_2018}/batch_instance-effective.csv ${dataPath_2018}/batch_instance-effective.csv.backup
+mkdir -p ${dataPath_2018}/midstFile
+make
+i=1
+j=1
+while [ $i -le $columnNum ]
+do
+	$currentPath/k_meanspp ${dataPath_2018}/batch_instance-effective.csv.backup ${j} $clusterNum
+	rm -f ${dataPath_2018}/batch_instance-effective.csv.backup
+	cp ${dataPath_2018}/clustered_column_${j}_based-batch_instance-effective.csv.backup ${dataPath_2018}/batch_instance-effective.csv.backup
+	mv ${dataPath_2018}/clustered_column_${j}_based-batch_instance-effective.csv.backup ${dataPath_2018}/midstFile/
+	head -300 ${dataPath_2018}/midstFile/clustered_column_${j}_based-batch_instance-effective.csv.backup >  ${dataPath_2018}/midstFile/clustered_column_${j}_based-batch_instance-effective.csv.backup-head-300.csv
+	i=`expr $i + 1`
+	j=`expr ${j} + 2`
+done
+awk -F , 'BEGIN{ OFS=","} { print $2,$4,$6,$8,$10  > "'${dataPath_2018}'/batch_instance-usable.csv"; } END{ print "Printed total lines = "NR}' ${dataPath_2018}/batch_instance-effective.csv.backup
+rm -f ${dataPath_2018}/batch_instance-effective.csv.backup
+
+
 ##2019-12-16-004
 #awk -F , 'END{print "set_1-lines="NR;}'  ${dataPath_2018}/randomNumFold/allSamples/set_1
 #awk -F , 'END{print "set_2-lines="NR;}'  ${dataPath_2018}/randomNumFold/allSamples/set_2
